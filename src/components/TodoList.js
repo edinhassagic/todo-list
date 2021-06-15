72ab5de022f2057f81260db011d355e709a8268b
edinhassagic <edinhassagic@gmail.com>
2021-06-15 16:52:26 +0200

    fixed focus,enterKey and mousepointer

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,8 @@ import TodoItem from "./TodoItem";
 function TodoList() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
-
+   
+  
   const saveData = (newTodos) => {
     localStorage.setItem("todos", JSON.stringify(newTodos));
   };
@@ -21,29 +22,35 @@ function TodoList() {
       setTodos(newTodos);
       setNewTodo("");
       saveData(newTodos);
-    }
-  };
+      }};
 
   const deleteTodo = (id) => {
     let newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
-
     saveData(newTodos);
   };
 
-  return (
-    
-<div className="container mt-5 text-info">
+
+
+  return(
+    <div className="container mt-5 text-info "> 
       <h2 className="fw-bold fs-1 ">ToDo List</h2>
       <input
         className="form-control form-control-lg text-center "
-        type="text"
-        placeholder="Add ToDo Item"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
+        autoFocus
+        type="text" 
+        name="forms" 
+        placeholder="Add To Do Item"
+        value={newTodo} 
+        onChange={(e) => setNewTodo(e.target.value)} 
+        onKeyPress={event => {
+          if (event.key === 'Enter') {
+            onAddTodo()
+          }
+        }}
+        />
 
-      <button className="btn btn-primary btn-block mt-2  " onClick={onAddTodo}>
+        <button className="btn btn-primary btn-block mt-2 " onClick={onAddTodo}>
         {" "}
         Add Here
       </button>
